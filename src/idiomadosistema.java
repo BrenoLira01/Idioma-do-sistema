873daeb74f13ae1f63414a9906b565af2933ec0e
BrenoLira01 <brenoczbreno@gmail.com>
2024-06-07 18:11:21 -0300

    primeiro commit

---
+++ src/idiomadosistema.java
@@ -0,0 +1,11 @@
+import java.util.Locale;
+
+public class idiomadosistema {
+    public static void main(String[] args) {
+
+        Locale locale = Locale.getDefault();
+        System.out.println(locale);
+
+    }
+
+}
